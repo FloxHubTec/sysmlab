8932fd90d09746c006e6d65be8dfe299804394d9
André Rapela <andre.d.q.rapela@gmail.com>
2025-12-15 12:15:00 -0300

    commit ambiente

---
+++ frontend/sysmlab/src/app/enviroments/enviroment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl:'https://exxufmvzgnbjmaexzmuz.supabase.co',
+  supabaseKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4eHVmbXZ6Z25iam1hZXh6bXV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3NDgzMTgsImV4cCI6MjA3NzMyNDMxOH0.R1lsXHhItc4LMLXSSUNYyfl4rt9J1RmLcgVHt1MgeUA"
+};
